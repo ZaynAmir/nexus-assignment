c8001972f28337a1cd6e4a33f80701d2ecb2c5d8
zaynamir <zaynsheikhofficial@gmail.com>
2024-07-11 22:14:05 +0500

    commented code clean

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,32 +1,3 @@
-// import React, { useEffect } from 'react';
-// import useUser from '../hooks/useUser';
-
-// export default function Home() {
-//     const { getUser, user, balance } = useUser();
-
-//     useEffect(() => {
-//         getUser();
-//     }, [getUser]); // Add getUser to the dependency array
-
-//     return (
-//         <div className='container mt-3'>
-//             <h2>
-//                 <div className='row'>
-//                     <div className="mb-12">
-//                         {user?.email !== undefined ? (
-//                             <>
-//                                 <div>Email: {user.email}</div>
-//                                 <div>Ethereum Wallet Balance: {balance ? balance : 5000} ETH</div>
-//                             </>
-//                         ) : (
-//                             'Please login first'
-//                         )}
-//                     </div>
-//                 </div>
-//             </h2>
-//         </div>
-//     );
-// }
 
 
 import React, { useEffect } from 'react';
